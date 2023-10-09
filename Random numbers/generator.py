67b29b3b2d287ec431de3fc7a94e665e9115e21a
SymonMuchemi <simonkenya846@gmail.com>
2023-10-09 11:49:45 +0300

    random  numbers

---
+++ Random numbers/generator.py
@@ -0,0 +1,15 @@
+import random
+
+random_number = random.randint(0, 9)
+random_number2 = random.random()
+uniformNumber = random.uniform(0, 9)
+sample = random.sample(range(0, 90), 12)
+rangeNumber = random.randrange(0, 9, 2)
+
+
+print(random_number)
+print(random_number2)
+print(uniformNumber)
+print(sample)
+print(sample.sort())
+print(rangeNumber)
