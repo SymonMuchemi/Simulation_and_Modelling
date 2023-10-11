79910d8330bcc2e13329880c8ae8a3afa2df9bb3
SymonMuchemi <simonkenya846@gmail.com>
2023-10-11 08:36:16 +0300

    test

--- Random numbers/generator.py
+++ Random numbers/generator.py
@@ -3,13 +3,16 @@ import random
 random_number = random.randint(0, 9)
 random_number2 = random.random()
 uniformNumber = random.uniform(0, 9)
-sample = random.sample(range(0, 90), 12)
+sample = random.sample(range(10, 20), 10)
 rangeNumber = random.randrange(0, 9, 2)         
+
+def myFunc(e):
+    return (e)
 
 
 print(random_number)
 print(random_number2)
 print(uniformNumber)
 print(sample)
-print(sample.sort())
+print(sample.sort(key=myFunc))
 print(rangeNumber)
